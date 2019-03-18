67183f70f01a7b06355ac041db42ed296cc01988
tim <cobalt111@hotmail.com>
2019-03-17 23:15:30 -0400

    added k=3 function, just need to verify 1d graphs

--- CSC546-HW6v2/src/main/kotlin/Driver.kt
+++ CSC546-HW6v2/src/main/kotlin/Driver.kt
@@ -4,6 +4,8 @@ import org.jfree.chart.JFreeChart
 import org.jfree.data.xy.XYDataset
 import org.jfree.data.xy.XYSeries
 import org.jfree.data.xy.XYSeriesCollection
+import org.jfree.util.ShapeUtilities
+import java.awt.Color
 import java.io.File
 import javax.swing.JFrame
 
@@ -31,57 +33,82 @@ fun main() {
     var data1D = Dataset(rawData1D, k, false)
     var data2D = Dataset(rawData2D, k, true)
 
-
-
-    class DataPlot(private val isTwoDimensional: Boolean) : JFrame() {
+    class DataPlot(private val isTwoDimensional: Boolean, private val k: Int): JFrame() {
 
         init {
-            var chart: JFreeChart = ChartFactory.createScatterPlot(
+            val chart: JFreeChart = ChartFactory.createScatterPlot(
                 if (isTwoDimensional) "HW6 Two Dimensions" else "HW6 One Dimension",
                 "X-Axis",
                 "Y-Axis",
                 convertToXYDataset(if (isTwoDimensional) data2D else data1D))
-            var panel = ChartPanel(chart)
+            chart.setTextAntiAlias(true)
+            chart.xyPlot.getRendererForDataset(chart.xyPlot.dataset).setSeriesPaint(0, Color.YELLOW)
+            chart.xyPlot.getRendererForDataset(chart.xyPlot.dataset).setSeriesPaint(1, Color.ORANGE)
+            chart.xyPlot.getRendererForDataset(chart.xyPlot.dataset).setSeriesPaint(2, Color.BLACK)
+            chart.xyPlot.getRendererForDataset(chart.xyPlot.dataset).setSeriesPaint(3, Color.RED)
+            chart.xyPlot.getRendererForDataset(chart.xyPlot.dataset).setSeriesPaint(4, Color.BLUE)
+            chart.xyPlot.getRendererForDataset(chart.xyPlot.dataset).setSeriesPaint(5, Color.GREEN)
+            chart.xyPlot.getRendererForDataset(chart.xyPlot.dataset).setSeriesShape(0, ShapeUtilities.createDiagonalCross(5f, 2f))
+            chart.xyPlot.getRendererForDataset(chart.xyPlot.dataset).setSeriesShape(1, ShapeUtilities.createDiamond(6f))
+            chart.xyPlot.getRendererForDataset(chart.xyPlot.dataset).setSeriesShape(2, ShapeUtilities.createRegularCross(5f, 2f))
+            val panel = ChartPanel(chart)
             contentPane = panel
         }
 
         fun convertToXYDataset(dataset: Dataset): XYDataset {
-            var newDataset = XYSeriesCollection()
-            var seriesOne = XYSeries("Cluster 1")
-            var seriesTwo = XYSeries("Cluster 2")
-            var seriesThree = XYSeries("Cluster 1 Centroid")
-            var seriesFour = XYSeries("Cluster 2 Centroid")
-
+            val newDataset = XYSeriesCollection()
+            val seriesOne = XYSeries("Cluster 1")
+            val seriesTwo = XYSeries("Cluster 2")
+            val seriesThree = XYSeries("Cluster 3")
+            val seriesFour = XYSeries("Cluster 1 Centroid")
+            val seriesFive = XYSeries("Cluster 2 Centroid")
+            val seriesSix = XYSeries("Cluster 3 Centroid")
             when (isTwoDimensional) {
                 true -> {
                     var currentCentroid = dataset.centroids[0]
                     for (point in currentCentroid.closestDataPoints) {
                         seriesOne.add(point.x, point.y)
                     }
-                    seriesThree.add(currentCentroid.coordinate.x, currentCentroid.coordinate.y)
+                    seriesFour.add(currentCentroid.coordinate.x, currentCentroid.coordinate.y)
                     currentCentroid = dataset.centroids[1]
                     for (point in currentCentroid.closestDataPoints) {
                         seriesTwo.add(point.x, point.y)
                     }
-                    seriesFour.add(currentCentroid.coordinate.x, currentCentroid.coordinate.y)
+                    seriesFive.add(currentCentroid.coordinate.x, currentCentroid.coordinate.y)
+                    if (k > 2) {
+                        currentCentroid = dataset.centroids[2]
+                        for (point in currentCentroid.closestDataPoints) {
+                            seriesThree.add(point.x, point.y)
+                        }
+                        seriesSix.add(currentCentroid.coordinate.x, currentCentroid.coordinate.y)
+                    }
                 }
                 false -> {
                     var currentCentroid = dataset.centroids[0]
                     for (point in currentCentroid.closestDataPoints) {
                         seriesOne.add(point.x, 0)
                     }
-                    seriesThree.add(currentCentroid.coordinate.x, currentCentroid.coordinate.y)
+                    seriesFour.add(currentCentroid.coordinate.x, currentCentroid.coordinate.y)
                     currentCentroid = dataset.centroids[1]
                     for (point in currentCentroid.closestDataPoints) {
                         seriesTwo.add(point.x, 0)
                     }
-                    seriesFour.add(currentCentroid.coordinate.x, currentCentroid.coordinate.y)
+                    seriesFive.add(currentCentroid.coordinate.x, currentCentroid.coordinate.y)
+                    if (k > 2) {
+                        currentCentroid = dataset.centroids[2]
+                        for (point in currentCentroid.closestDataPoints) {
+                            seriesThree.add(point.x, point.y)
+                        }
+                        seriesSix.add(currentCentroid.coordinate.x, currentCentroid.coordinate.y)
+                    }
                 }
             }
+            newDataset.addSeries(seriesFour)
+            newDataset.addSeries(seriesFive)
+            newDataset.addSeries(seriesSix)
             newDataset.addSeries(seriesOne)
             newDataset.addSeries(seriesTwo)
             newDataset.addSeries(seriesThree)
-            newDataset.addSeries(seriesFour)
             return newDataset
         }
     }
@@ -91,8 +118,9 @@ fun main() {
         data2D.updateCentroidMeans()
     }
 
-    val plot1D = DataPlot(false)
-    val plot2D = DataPlot(true)
+    // Plot the datasets
+    val plot1D = DataPlot(false, k)
+    val plot2D = DataPlot(true, k)
     plot1D.setSize(800, 500)
     plot1D.show()
     plot2D.setSize(800, 500)
